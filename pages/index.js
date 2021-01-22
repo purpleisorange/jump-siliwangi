7612ad4d8d539893ca119785a512ca9536f8388d
Siswanto <siswantosmik@gmail.com>
2021-01-22 16:35:23 +0700

    modified:   pages/index.js 	new file:   public/Free.png 	new file:   public/Illustration1.png 	new file:   public/Illustration2.png 	new file:   public/Logo.png 	new file:   public/Server.png 	new file:   public/amazon.png 	new file:   public/discord.png 	new file:   public/group.png 	new file:   public/hugeglobal.svg 	new file:   public/location.png 	new file:   public/netflix.png 	new file:   public/reddit.png 	new file:   public/spotify.png 	new file:   public/user.png 	modified:   styles/Home.module.css

--- pages/index.js
+++ pages/index.js
@@ -7,62 +7,157 @@ export default function Home() {
   const [halo, ds] = useState('Halo Next')
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>{halo}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className={styles.template}>
+      <header>
+        <div className={styles.headerWrap}>
+          <div className={styles.headerLeft}>
+            <img src="/Logo.png"></img>
+          </div>
+          <div className={styles.headerRight}>
+            <a href="#about">About</a>
+            <a href="#features">Features</a>
+            <a href="#pricing">Pricing</a>
+            <a href="#testimonials">Testimonials</a>
+            <a href="#help">Help</a>
+            <button className={styles.SignIn} type="button">Sign In</button>
+            <button className={styles.SignUp} type="button">Sign Up</button>
+          </div>
+        </div>
+      </header>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to {halo} <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <section>
+        <div className={styles.container} id="about">
+          <div className={styles.aboutWrap}>  
+            <div className={styles.aboutLeft}>
+              <h1>
+                Want anything to be easy with <span>LaslesVPN.</span>
+              </h1>
+              <p>
+                Provide a network for all your needs with ease and fun using <span>LaslesVPN</span> discover interesting features from us.
+              </p>
+              <button className={styles.btn} type="button"> Get Started </button>
+            </div>
+            <div className={styles.aboutRight}>
+              <img className={styles.img} src="/Illustration1.png" alt="Illustration1"></img>
+            </div>
+          </div>
+        </div>
+      </section>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <main>
+        <div className={styles.container} id="features">
+          <div className={styles.detailWrap}>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          </div>
+          <div className={styles.featuresWrap}>
+            <div className={styles.featuresLeft}>
+              <img src="/Illustration2.png"></img>
+            </div>
+            <div className={styles.featuresRight}>
+              <h1>
+                We Provide Many Features You Can Use
+              </h1>
+              <p>
+                You can explore the feature thah we provide with fun and heva their own functions each feature.
+              </p>
+              <ul>
+                <li> Powerfull online protection.</li>
+                <li> Internet without border.</li>
+                <li> Supercharger VPN.</li>
+                <li> No specific time limits.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className={styles.pricingWrap} id="pricing">
+          <h1>Choose Your Plan</h1>
+          <p>Lets's choose the package that is best for you and explore it happily and cheerfully.</p>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <div className={styles.pricing}>
+            <div>
+              <img src="/Free.png"></img>
+              <h1>Free Plan</h1>
+              <ul>
+                <li> Unlimites Bandwitch</li>
+                <li> Encrypted Connection</li>
+                <li> No Traffic Logs</li>
+                <li> Works on All Devices</li>
+              </ul>
+              <h2>Free</h2>
+              <button type="button">Select</button>
+            </div>
+            <div>
+              <img src="/Free.png"></img>
+              <h1>Standart Plan</h1>
+              <ul>
+                <li> Unlimites Bandwitch</li>
+                <li> Encrypted Connection</li>
+                <li> Yes Traffic Logs</li>
+                <li> Works on All Devices</li>
+                <li> Connect Anyware</li>
+              </ul>
+              <h2>$9 / mo</h2>
+              <button type="button">Select</button>
+            </div>
+            <div>
+              <img src="/Free.png"></img>
+              <h1>Premium Plan</h1>
+              <ul>
+                <li> Unlimites Bandwitch</li>
+                <li> Encrypted Connection</li>
+                <li> Yes Traffic Logs</li>
+                <li> Works on All Devices</li>
+                <li> Connect Anyware</li>
+                <li> Get New Features</li>
+              </ul>
+              <h2>$12 / mo</h2>
+              <button type="button">Select</button>
+            </div>
+          </div>
+        </div>
+        <div className={styles.testimonialsWrap} id="testimonials">
+          <h1>
+            Huge Global Network of Fast VPN
+          </h1>
+          <p>
+            See <span>LaslesVPN</span> everywhere to make it easier for you when you move locations.
+          </p>
+          <img src="/hugeglobal.svg"></img>
+          <ul>
+            <li><img src="/netflix.png"></img></li>
+            <li><img src="/reddit.png"></img></li>
+            <li><img src="/amazon.png"></img></li>
+            <li><img src="/discord.png"></img></li>
+            <li><img src="/spotify.png"></img></li>
+          </ul>
+        </div>
+        
+        <div className={styles.sliderWrap}>
+          <h1>
+            Trusted by Thousands of Happy Customer
+          </h1>
+          <p>
+          These are the stories of our customers who have joined us with great pleasure when using this crazy feature.
+          </p>
+        </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
+        <div className={styles.subscribeWrap}>
+          <div>
+            <h1>
+              Subscribe Now for Get Special Features!
+            </h1>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Let's subscribe with us and find the fun.
             </p>
-          </a>
+          </div>
+          <div>
+            <button type="button">Subscribe Now</button>
+          </div> 
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+      <footer>
+
       </footer>
     </div>
   )
