5804bbd5eecea0d424860ecbd9bd7e9ec736c43f
restufff <restuff666@gmail.com>
2021-01-22 15:50:35 +0700

    Menambahkan hover pada button dan styling pada section features

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Link from 'next/link';
 function Home() {
     return (
       <div className = "container">
+
+        {/*Navbar*/}
+        
         <nav>
           <Link href="/">
             <a className = "logo"><img src = "/Logo.svg" alt="LasslesVPN"></img></a></Link>
@@ -18,10 +21,12 @@ function Home() {
                 <button className = "cta-red">Sign Up</button>
               </div>
         </nav>
-    
+        
+        {/*Hero*/}
+
         <section className = "hero">
           <div className = "column-text-heading">
-            <p className = "hero-heading">Want anything to be <br></br> easy with <strong >LaslesVPN.</strong></p>
+            <h1 className = "hero-heading">Want anything to be <br></br> easy with <span >LaslesVPN.</span></h1>
             <p className = "text-p">
             Provide a network for all your needs with ease and fun using <strong>LaslesVPN</strong> <br></br>discover interesting features from us.
             </p>
@@ -30,6 +35,37 @@ function Home() {
           <div>
             <img className = "illustration-hero" src = "/illust.png"></img>
           </div>
+        </section>
+        
+        {/*Features*/}
+
+        <section className = "features">
+          <div className = "row-column">
+            <div className = "column">
+              <img src="/user.png"></img>
+              <div className = "text-col">
+                <h1>90+</h1>
+                <p>Users</p>
+              </div>
+              <img src = "/gap.png"></img>
+            </div>
+            <div className = "column">
+              <img src="/location.png"></img>
+              <div className = "text-col">
+                <h1>30+</h1>
+                <p>Locations</p>
+              </div>
+              <img src = "/gap.png"></img>
+            </div>
+            <div className = "column">
+              <img src="/server.png"></img>
+              <div className = "text-col">
+                <h1>50+</h1>
+                <p>Servers</p>
+              </div>
+            </div>
+          </div>
+
         </section>
       </div>   
     )
